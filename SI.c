68bbceae4288f8c5552cb3e7cad8f9c3932513dc
Gulshan Godsora <gulshangodsora008@gmail.com>
2024-11-02 21:18:22 +0530

    simple interest

--- SI.c
+++ SI.c
@@ -6,7 +6,7 @@ printf("Enter Principal Amount: ");
 scanf("%f",&P);
 printf("Enter Interest Rate: ");
 scanf("%f",&R);
-printf("Enter Duration: ");
+printf("Enter Duration in Years: ");
 scanf("%f",&T);
     float SI = (P*R*T)/100;
     printf("Simple Interest = %0.4f\n", SI);
